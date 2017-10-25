67e0c558b6222ce81e293cb30705b874af6639db
Unknown <pe0877888@otc.edu>
2017-10-25 13:16:05 -0500

    work in progress

---
+++ race.js
@@ -0,0 +1,16 @@
+$(function(){
+ 
+    racing: false;
+    winner: null;
+    racer1: 0;
+    racer2; 0;
+    intervall: null;
+
+    
+  function startbtn(){
+     document.getElementById("demo").innerHTML = Math.random();
+
+  }
+
+
+})
