8c50a80eb5acbd70a2f119b0454ab0e1d1e369fa
Unknown <pe0877888@otc.edu>
2017-10-31 19:09:22 -0500

    almost

--- race.js
+++ race.js
@@ -1,23 +1,91 @@
 $(function(){
- 
+   
      let vm = new Vue({
+        el: '#begin',
+         data: {
         racing: false,
         winner: null,
-        racer1: 0,
-        racer2: 0,
+        bo: 0,
+        otherbo: 0,
+        tick: 0,
         intervall: null,
+         },
+        methods: {
 
-        method: {
-            letsRace(){
-               intervall = Math.random()
+            letsRace() {
+                if (this.winner) {
+                    this.dueOver()
+                    return
+                }
+                this.racing = true
+                this.interval = setInterval(() => {
+                    this.moveRacer()
+
+                }, 90)
             },
 
-           
+            moveRacer() {
+                this.tick++
+                this.bo += (Math.random() >= Math.random()) ? 1 : 0
+                this.otherbo += (Math.random() >= Math.random()) ? 1 : 0
+                this.whoWon()
+            },
+
+            whoWon() {
+                if (this.bo == this.otherbo) return
+                if (this.bo >= 85) {
+                    this.Winning('bo')
+                }
+                if (this.otherbo >= 85) {
+                    this.Winning('otherbo')
+                }
+            },
+
+            Winning(player) {
+                clearInterval(this.interval)
+                this.interval = null
+                this.racing = false
+                this.winner = player
+            },
+
+            dueOver() {
+                this.racing = false
+                this.winner = null
+                this.bo = 0
+                this.otherbo = 0
+                this.tick = 0
+            }
         },
 
         computed: {
 
-        }
+            boStyles() {
+                return {
+                    left: `${this.bo}vw`
+                }
+            },
+
+            boClass() {
+                if (!this.winner) return
+                return this.winner == 'bo'
+            },
+
+            otherboStyles() {
+                return {
+                    left: `${this.otherbo}vw`
+                }
+            },
+
+            otherBoClass() {
+                if (!this.winner) return
+                return this.winner == 'otherbo'
+            },
+
+            winner() {
+                if (this.bo == this.otherbo) return null
+                    return this.bo > this.otherbo ? 'bo' : 'otherbo'
+            }
+        },
 
 
 
